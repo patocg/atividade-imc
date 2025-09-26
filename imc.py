958e60d0a302bb8590f09929029b4612e0c82269
Jonathas Cunha <jlcunha@live.com>
2025-09-26 03:39:58 -0400

    Adicionado o program Python para o calculo do IMC

---
+++ imc.py
@@ -0,0 +1,23 @@
+# Programa para calcular o IMC (Índice de Massa Corporal)
+
+# Fórmula de cálculo: IMC = Peso / (Altura * Altura)
+
+# Solicita peso e altura do usuário
+peso = float(input("Digite seu peso: ")) # ex: 87.5
+altura = float(input("Digite sua altura: ")) # ex: 1.82
+
+# Calculo do IMC
+imc = peso / (altura ** 2)
+
+# Mostra o resultado com duas casas decimais
+print(f"Seu IMC é: {imc:.2f}")
+
+# Classifica o resultado do IMC
+if imc < 18.5:
+    print ("Abaixo do Peso")
+elif imc < 25:
+    print("Peso Normal")
+elif imc > 30:
+    print("Obesidade")
+else:
+    print("Sobrepeso")
